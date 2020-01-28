77d83ea0f8e685204c0ee1d8b046a61c77841cd9
Hector R <birkoff.h@gmail.com>
2020-01-28 22:29:22 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -44,33 +44,33 @@ module "vpc" {
 }
 
 output "vpc_id" {
-  value = "${module.eiq-vpc.vpc_id}"
+  value = "${module.vpc.vpc_id}"
 }
 
 output "public_subnets" {
-  value = "${module.eiq-vpc.public_subnets}"
+  value = "${module.vpc.public_subnets}"
 }
 
 output "public_subnets_cidr_blocks" {
-  value = "${module.eiq-vpc.public_subnets_cidr_blocks}"
+  value = "${module.vpc.public_subnets_cidr_blocks}"
 }
 
 output "public_route_table_ids" {
-  value = "${module.eiq-vpc.public_route_table_ids}"
+  value = "${module.vpc.public_route_table_ids}"
 }
 
 output "database_subnets_cidr_blocks" {
-  value = "${module.eiq-vpc.database_subnets_cidr_blocks}"
+  value = "${module.vpc.database_subnets_cidr_blocks}"
 }
 
 output "private_subnets_cidr_blocks" {
-  value = "${module.eiq-vpc.private_subnets_cidr_blocks}"
+  value = "${module.vpc.private_subnets_cidr_blocks}"
 }
 
 output "private_subnets" {
-  value = "${module.eiq-vpc.private_subnets}"
+  value = "${module.vpc.private_subnets}"
 }
 
 output "database_subnets" {
-  value = "${module.eiq-vpc.database_subnets}"
+  value = "${module.vpc.database_subnets}"
 }
